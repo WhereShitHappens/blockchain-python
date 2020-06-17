7464dfdaa4b262ee6e1be438889dc1d79b839408
Aris <norbuaris@gmail.com>
2020-06-17 22:37:53 +0300

    blockchain manipulation

--- blockchain.py
+++ blockchain.py
@@ -4,56 +4,85 @@ blockchain = []
 
 def get_last_blockchain_value():
     """ Returns the last value of the current blockchain."""
+    if len(blockchain) < 1:
+        return None
+    # We could have used 'else', but return by its nature can only run once
     return blockchain[-1]
 
-
-def add_value(transaction_amount, last_transaction=[1]):
+# We don't need the '=[1]' default argument since line 20, it is just for reference
+def add_transaction(transaction_amount, last_transaction=[1]):
     """ Append a new value as well as the last blockchain value to the blockchain
 
-     Aruments:
+     Arguments:
          :transaction_amount: The amount that should be added.
          :last_transaction: The last blockchain transaction (default [1])
      """
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_input():
-    """ Returns the input of the user (a new transction amount) as a float"""
+def get_transaction_value():
+    """ Returns the input of the user (a new transaction amount) as a float"""
+    # Get the user input, transform it from a string to a float and store it
     user_input = float(input('Your transaction amount please: '))
     return user_input
 
 
-tx_amount = get_user_input()
-add_value(tx_amount)
+def get_user_choice():
+    user_input = input('Your choice: ')
+    return user_input
 
-# tx_amount = get_user_input()
-add_value(last_transaction=get_last_blockchain_value(), transaction_amount=get_user_input())
 
-tx_amount = get_user_input()
-add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
+def print_blockchain_elements():
+    # Output the blockchain list to the console
+    for block in blockchain:
+        print('Outputting Block')
+        print(block)
 
-print(blockchain)
+def verify_chain():
+    block_index = 0
+    is_valid = True
+    for block in blockchain:
+        if block_index == 0:
+            block_index += 1
+            continue
+        print(block, 'Counting blocks')
+        if block[0] == blockchain[block_index - 1]:
+            is_valid = True
+        else:
+            is_valid = False
+            break
+        block_index += 1
+    return is_valid
 
-# # Global local variables
-# name  = 'Aris'
-# def get_name():
-# 	name = input("Your name please:")
-# get_name()
-# print(name)
+while True:
+    print('Please choose')
+    print('1: Add a new transaction value')
+    print('2: Output the blockchain blocks')
+    print('h: Manipulate the chain')
+    print('q: Quit')
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_transaction(tx_amount, get_last_blockchain_value())
+    elif user_choice == '2':
+        print_blockchain_elements()
+    elif user_choice == 'h':
+        if len(blockchain) >= 1:
+            blockchain[0] = [2]
+    elif user_choice == 'q':
+        # Using 'continue' instead would stop the code at the current line and rerun the loo[
+        break
+    else:
+        print('Input was invalid, please pick a value from the list!')
+    print('Choice registered!')
+    # If not true
+    if not verify_chain():
+        print('Invalid blockchain!')
+        break
 
-# def greet(name, age =29):
-# 	print('Hellot ' + 'me name is ' + name + ', I am ' + str(age) )
-# greet('aris')
 
-# # Keyword arguments
-# def greet(name, age):
-# 	print('My name is ' + name + ' and I am ' + str(age))
-# greet(age=29, name='Aris')
+print('Done!')
 
-# fucking hell this is a fucking nightmare
-# I cant even describe it because it wouldn't feel like a nightmare in words
-# this is some real bullshit
-# its not like I have not been trying 
-# Is it not enough? Maybe
-# Can I try harder? Maybe
-# Will this shit ever end? For sure
+# 48
